5c7fb2a5c77fa57f1f3382a1a4a4fe1f7d7535b0
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2019-08-17 10:59:57 +0900

    Add env to docker node

--- fish/functions/docker_node.fish
+++ fish/functions/docker_node.fish
@@ -1,6 +1,8 @@
 function docker_node
   sudo docker run --rm -it --name node-docker \
     -v $PWD:/home/app -w /home/app \
+    -e "NPM_CONFIG_PREFIX=/home/node/.npm-global" \
+    -e "PATH=$PATH:/home/node/.npm-global/bin" \
     $argv[2..-1] \
     -u node node:$argv[1] /bin/sh
 end
